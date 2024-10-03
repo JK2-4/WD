795fc8193b5f0a4f81c1a2e3f16fa46c5ea6efad
JK2-4 <76730223+JK2-4@users.noreply.github.com>
2024-10-03 17:34:09 +0800

    Update Vine Copula model.R

--- DATA_weather/Vine Copula model.R
+++ DATA_weather/Vine Copula model.R
@@ -1,9 +1,10 @@
+###################################################################### lib
 library(devtools)
 devtools::install_github("tnagler/VC2copula")
 library(VC2copula)
 
 # Define required packages
-required_packages <- c("copula", "ggplot2", 
+required_packages <- c("VineCopula","copula", "ggplot2", 
   "dplyr", "lubridate", "tidyr", 
   "networkD3", "igraph", "httr"
 )
@@ -19,6 +20,7 @@ if (!require(devtools)) {
   install.packages("devtools")
 }
 
+library(VineCopula)
 library(copula)
 library(ggplot2)
 library(dplyr)
@@ -80,4 +82,67 @@ pairs(vars[, 1:4])
 vine <- vineCopula(as.integer(4))
 fit <- fitCopula(vine, vars[, 1:4])
 pairs(rCopula(500, fit@copula))
+
+######################################################################  Results 
+# Fit a vine copula model (R-vine)
+# familyset = 1:10 allows for various bivariate copula families
+vine_model <- RVineStructureSelect(u, familyset = 1:10, type = "RVine")
+print(vine_model)
+
+variable_names <- c("tavg", "prcp", "wspd_anomaly", "wdir_sin", "wdir_cos", "pres")
+# Number of variables
+d <- length(variable_names)
+
+# Initialize matrices
+Matrix <- matrix(0, nrow = d, ncol = d)
+family <- matrix(0, nrow = d, ncol = d)
+par <- matrix(NA, nrow = d, ncol = d)
+par2 <- matrix(NA, nrow = d, ncol = d)
+
+# Assign row and column names for clarity
+rownames(Matrix) <- variable_names
+colnames(Matrix) <- variable_names
+rownames(family) <- variable_names
+colnames(family) <- variable_names
+rownames(par) <- variable_names
+colnames(par) <- variable_names
+rownames(par2) <- variable_names
+colnames(par2) <- variable_names
+
+names <- variable_names
+
+# Create the RVineMatrix object
+RVM <- RVineMatrix(
+  Matrix = vine_model$Matrix,
+  family = vine_model$family,
+  par = vine_model$par,
+  par2 = vine_model$par2,
+  names = names,
+  check.pars = TRUE
+)
+
+# View the RVineMatrix object
+print(RVM)
+
+#simulate data based on copula 
+# Simulate 1000 observations from the fitted vine copula
+simulated_data <- RVineSim(1000, vine_model)
+head(simulated_data)
+
+# Extract the vine structure matrix
+vine_structure <- vine_model$Matrix
+print(vine_structure)
+
+# Extract the copula families matrix
+copula_families <- vine_model$family
+print(copula_families)
+
+# Extract the first parameter matrix
+copula_par1 <- vine_model$par
+copula_par2 <- vine_model$par2
+print(copula_par1)
+print(copula_par2)
+
+###################################################################### test log likelihood 
+RVineLogLik(u, RVM, par = RVM$par, par2 = RVM$par2,separate = FALSE, verbose = TRUE, check.pars = TRUE, calculate.V = TRUE)
 
